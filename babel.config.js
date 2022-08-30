84bb16a40daca65a10ddda7532d1035fa4a571a9
JEFFERSON BEZERRA DE LIMA <JEFFERSON.LIMA@recibom.corp>
2022-08-30 15:52:02 -0300

    first commit

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+  };
+};
+
+/*
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    //presets: ['babel-preset-expo']
+    presets: ["module:metro-react-native-babel-preset"],
+    plugins: ['react-native-reanimated/plugin'],
+  env: {
+    production: {
+      plugins: ['react-native-paper/babel'],
+    },
+  },
+  };
+};*/
